de6df3bcd9aee251a271941889bdfbf313ed69fe
Pelayo <pelayo.pvt@gmail.com>
2024-06-01 14:52:22 +0200

    Mapa inicial

--- inmaps/lib/pages/map_page.dart
+++ inmaps/lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +9,17 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _pGooglePlex,
+           zoom: 13,
+           ),
+          ),
+        );
+
   }
 }
